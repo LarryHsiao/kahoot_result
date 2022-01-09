6330b6f930f0472ffa28abab828ccfb18716ab1d
Larry Hsiao <larryhsiao@larryhsiao.com>
2022-01-09 23:52:06 +0800

    Use water.css framework

--- src/main/java/com/larryhsiao/kahoot_result/takes/KahootReportTake.java
+++ src/main/java/com/larryhsiao/kahoot_result/takes/KahootReportTake.java
@@ -1,6 +1,5 @@
 package com.larryhsiao.kahoot_result.takes;
 
-import com.larryhsiao.kahoot_result.Main;
 import com.larryhsiao.kahoot_result.players.PlayersImpl;
 import com.larryhsiao.kahoot_result.players.ScoredPlayers;
 import com.larryhsiao.kahoot_result.scores.KahootScores;
@@ -9,14 +8,10 @@ import org.takes.Request;
 import org.takes.Response;
 import org.takes.Take;
 import org.takes.rq.multipart.RqMtBase;
-import org.takes.rs.RsHtml;
 import org.takes.rs.RsVelocity;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 
 public class KahootReportTake implements Take {
     @Override
@@ -33,21 +28,25 @@ public class KahootReportTake implements Take {
             ).all().stream().sorted(
                 (t1, t2) -> (int) ((t2.totalScore() * 100) - (t1.totalScore() * 100))
             ).forEach(team -> {
-                stringBuilder.append("team ").append(team.name()).append(", ")
-                    .append(String.valueOf(team.players().size())).append(" players, totalScore: ")
-                    .append(String.valueOf(team.totalScore()));
-                stringBuilder.append("\n");
+                stringBuilder.append("<table>");
+                stringBuilder.append("<thead><tr><th colspan=\"3\">");
+                stringBuilder.append("team ")
+                    .append(team.name())
+                    .append(", ")
+                    .append(team.players().size())
+                    .append(" players, totalScore: ")
+                    .append(team.totalScore());
+                stringBuilder.append("</th></tr></thead>");
+                stringBuilder.append("<tbody>");
                 team.players().forEach(player -> {
-                    stringBuilder.append(player.id());
-                    stringBuilder.append(" ");
-                    stringBuilder.append(player.name());
-                    stringBuilder.append(" ");
-                    stringBuilder.append(player.email());
-                    stringBuilder.append(" ");
-                    stringBuilder.append(player.score());
-                    stringBuilder.append("\n");
+                    stringBuilder.append("<tr>");
+                    stringBuilder.append("<td>").append(player.id()).append("</td>");
+                    stringBuilder.append("<td>").append(player.name()).append("</td>");
+                    stringBuilder.append("<td>").append(player.score()).append("</td>");
+                    stringBuilder.append("</tr>");
                 });
-                stringBuilder.append("\n");
+                stringBuilder.append("</tbody>");
+                stringBuilder.append("</table>");
             });
             return new RsVelocity(
                 getClass().getResource("/result.html.vm"),
